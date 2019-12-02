bffce119770300706c669c3b68bc2208a8fcc521
root <root@localhost.localdomain>
2019-12-02 13:32:15 -0500

    Fixed script2

--- script2.bash
+++ script2.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-#testing
+
 # StoryLine:
 
 ###### PUT A COMMENT ABOVE EACH BUG YOU FIXED.  PREFIX IT WITH:
@@ -7,25 +7,26 @@
 
 # 101.236.44.127 - - [24/Oct/2017:04:11:14 -0500] "GET / HTTP/1.1" 200 225 "-" "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36"
 
-# 
-sed -e 's/\[//g' -e 's/\]//g' -e 's/"/""/g' access.log |\
-
-awk " BEGIN { 
+#BUG: Fixed third sed statement to remove double quotes
+sed -e 's/\[//g' -e 's/\]//g' -e 's/\"//g' access.log |\
+#BUG: Changed double quote to match closing single quote
+awk ' BEGIN {
 		print "IP," "Date," "Method," "URI," "User Agent"
 	}
 		{
 		msg=""
 
-		# 
-		for (i = 12 : i <= NF : i++)		
+		#BUG: Changed : to ;
+		for (i = 12 ; i <= NF ; i++)
 			msg = msg " " $i;
-			
-		#
+
+		#BUG: changed / to \
 		q = "\""
 
-		# 
+		#BUG: Changed / to \
 		cq = "\","
 
 		# Print the output
-		print q$1cq q$4cq q$6cq q$7cq (q msg ecq)
+		#BUG: Changed "ecq" to "cq"
+		print q$1cq q$4cq q$6cq q$7cq (q msg cq)
 		} '  | sed 's/," /,"/g' >> access.csv
